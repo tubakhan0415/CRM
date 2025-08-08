62de44d2d5cc1d544615c962f8eb4947d195053c
Tuba_Khan <121151121+tubakhan0415@users.noreply.github.com>
2025-08-08 16:11:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+app.listen(5000, '0.0.0.0', () => {
+  console.log("Server running on port 5000");
 });
+
