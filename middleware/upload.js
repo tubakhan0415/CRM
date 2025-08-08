06d13f55e8354f4c3560a7c4d9245e485e3a7997
Tuba_Khan <121151121+tubakhan0415@users.noreply.github.com>
2025-08-08 15:28:19 +0530

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,7 +2,6 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-financials
   destination: (req, file, cb) => cb(null, "uploads"),
   filename: (req, file, cb) => {
     const uniqueName = `project-${Date.now()}${path.extname(file.originalname)}`;
